440b1d952f00409cb3779ee5d16a32139b9965ac
zhangyunhao06 <zeme56@163.com>
2024-07-16 08:06:51 +0800

    枚举常量.cpp

---
+++ 枚举常量.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+enum math
+{
+	c,
+	a,
+	b
+
+};
+int main()
+{
+	//enum math m = c;
+		printf("%d\n", c);
+	printf("%d\n", a);
+	printf("%d\n", b);
+	return 0;
+}
